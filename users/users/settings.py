b701fba5471c31cf387cefa0abcbaea1e79b0ccf
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-03-01 16:21:17 +0530

    DB changes to shubham-local

--- users/users/settings.py
+++ users/users/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'root',
-        'HOST':'127.0.0.1',
+        'HOST':'localhost',
         'PORT':'3306',
         'USER':'shubham',
         'PASSWORD':'12345',
