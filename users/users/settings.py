74c162b6c72094815c88b99819097e009f62e13b
ShubhamPrasad-TransEV <transmogrify11@outlook.com>
2024-03-01 16:19:37 +0530

    DB changes to shubham-local

--- users/users/settings.py
+++ users/users/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'users.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'u179156391_Transcrm',
-        'HOST':'trans-crm.cpaucm8a4ch4.ap-south-1.rds.amazonaws.com',
+        'NAME': 'root',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
-        'USER':'u179156391_admin',
-        'PASSWORD':'Tgpl@2024',
+        'USER':'shubham',
+        'PASSWORD':'12345',
     }
 }
 
